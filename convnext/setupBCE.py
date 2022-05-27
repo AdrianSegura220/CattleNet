29bdc197c8ad7cd1982d24d8418bc890c40376ad
AdrianSegura220 <adrianseglor@gmail.com>
2022-05-27 13:38:49 +0200

    overfit

--- convnext/setupBCE.py
+++ convnext/setupBCE.py
@@ -96,7 +96,7 @@ def train():
     epoch_loss = 0.0
     iterations_loop = 0
     # create directory for current training results
-    final_path = os.path.join(path_to_results,'CattleNetV3_WValidationWorking_lr{}_BCE_datetime{}-{}H{}M{}S{}'.format(lr,datetime.datetime.today().day,datetime.datetime.today().month,datetime.datetime.today().hour,datetime.datetime.today().minute,datetime.datetime.today().second))
+    final_path = os.path.join(path_to_results,'CattleNetV3_Overfit_lr{}_BCE_datetime{}-{}H{}M{}S{}'.format(lr,datetime.datetime.today().day,datetime.datetime.today().month,datetime.datetime.today().hour,datetime.datetime.today().minute,datetime.datetime.today().second))
     os.mkdir(final_path)
     last_epoch = 0
     for epoch in range(1,num_epochs):
