6bb6801358e09a60fa06aab8b803372b882b3182
AdrianSegura220 <adrianseglor@gmail.com>
2022-06-01 19:07:49 +0200

    hotfix

--- convnext/setup.py
+++ convnext/setup.py
@@ -84,8 +84,8 @@ def train(d_loader,dataset_validation):
     epoch_loss = 0.0
     iterations_loop = 0
     # create directory for current training results
-    final_path = os.path.join(path_to_results,'CattleNetContrastive_lr{}_BCE_datetime{}-{}H{}M{}S{}'.format(lr,datetime.datetime.today().day,datetime.datetime.today().month,datetime.datetime.today().hour,datetime.datetime.today().minute,datetime.datetime.today().second))
-    os.mkdir(final_path)
+    # final_path = os.path.join(path_to_results,'CattleNetContrastive_lr{}_BCE_datetime{}-{}H{}M{}S{}'.format(lr,datetime.datetime.today().day,datetime.datetime.today().month,datetime.datetime.today().hour,datetime.datetime.today().minute,datetime.datetime.today().second))
+    # os.mkdir(final_path)
 
     for epoch in range(1,num_epochs):
         loop = tqdm(d_loader,leave=False,total=len(d_loader))
