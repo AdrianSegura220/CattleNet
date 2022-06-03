32443cdf4bfdc3e8540a6fb59b462f0c5433a75f
AdrianSegura220 <adrianseglor@gmail.com>
2022-06-03 23:24:35 +0200

    hotfix

--- convnext/model_test_original.py
+++ convnext/model_test_original.py
@@ -202,6 +202,7 @@ def one_shot_test(test_dataset: OneShotImageDataset,model,threshold):
     # generate all the embeddings and store them
     for data in data_loader:
         if int(data[1]) not in images:
+            print(int(data[1]))
             images[int(data[1])] = []
             
         img = data[0].to(device)
