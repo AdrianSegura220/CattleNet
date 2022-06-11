cfd465285384ed328b777872fbbd8171093de232
AdrianSegura220 <adrianseglor@gmail.com>
2022-06-11 15:10:19 +0200

    docs: debug

--- convnext/setup.py
+++ convnext/setup.py
@@ -106,7 +106,7 @@ def train(d_loader,dataset_validation):
         epoch_loss = 0.0
         iterations_loop = 0
         for data in loop:
-            print(len(data))
+            print(data[0].size())
             label = 0
             #### do something with images ...
             optimizer.zero_grad()
