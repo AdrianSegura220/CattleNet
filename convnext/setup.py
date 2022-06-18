514783436377f8e9872605aa62b6c9d8632c7645
AdrianSegura220 <adrianseglor@gmail.com>
2022-06-18 22:55:46 +0200

    docs: changed use_wandb to true

--- convnext/setup.py
+++ convnext/setup.py
@@ -39,7 +39,7 @@ save_models = False
 save_figs = False
 
 # wandb setup (logging progress to online platform)
-use_wandb = False
+use_wandb = True
 
 if use_wandb:
     wandb.init(project="cattleNet-arch1", entity="adriansegura220")
